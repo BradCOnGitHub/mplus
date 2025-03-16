d687dff0e892fe94b6cd56d4c068f871514963e1
Ben Burgess <wburgess@booksys.com>
2025-03-16 13:19:32 -0500

    forgot to update dungeon loot track

--- docs/index.js
+++ docs/index.js
@@ -36,16 +36,16 @@
     ];
 
     const KEY_ILVL_MAP = {
-        '0':  { loot: 636, crest: 'Carved (15)', dtrack:'Veteran 8/8',  vault: 645, vtrack:'Champion 4/8' },
-        '2':  { loot: 639, crest: 'Runed (10)', dtrack:'Champion 1/8', vault: 649, vtrack:'Hero 1/6' },
-        '3':  { loot: 639, crest: 'Runed (12)', dtrack:'Champion 1/8', vault: 649, vtrack:'Hero 1/6' },
-        '4':  { loot: 642, crest: 'Runed (14)',  dtrack:'Champion 2/8', vault: 652, vtrack:'Hero 2/6' },
-        '5':  { loot: 645, crest: 'Runed (16)',  dtrack:'Champion 3/8', vault: 652, vtrack:'Hero 2/6' },
-        '6':  { loot: 649, crest: 'Runed (18)',  dtrack:'Champion 4/8', vault: 655, vtrack:'Hero 3/6' },
+        '0':  { loot: 636, crest: 'Carved (15)', dtrack:'Champion 1/8',  vault: 645, vtrack:'Champion 4/8' },
+        '2':  { loot: 639, crest: 'Runed (10)', dtrack:'Champion 2/8', vault: 649, vtrack:'Hero 1/6' },
+        '3':  { loot: 639, crest: 'Runed (12)', dtrack:'Champion 2/8', vault: 649, vtrack:'Hero 1/6' },
+        '4':  { loot: 642, crest: 'Runed (14)',  dtrack:'Champion 3/8', vault: 652, vtrack:'Hero 2/6' },
+        '5':  { loot: 645, crest: 'Runed (16)',  dtrack:'Champion 4/8', vault: 652, vtrack:'Hero 2/6' },
+        '6':  { loot: 649, crest: 'Runed (18)',  dtrack:'Hero 1/6    ', vault: 655, vtrack:'Hero 3/6' },
         '7':  { loot: 649, crest: 'Gilded (10)', dtrack:'Hero 1/6    ', vault: 658, vtrack:'Hero 4/6' },
-        '8':  { loot: 652, crest: 'Gilded (12)', dtrack:'Hero 1/6    ', vault: 658, vtrack:'Hero 4/6' },
-        '9':  { loot: 652, crest: 'Gilded (14)', dtrack:'Hero 1/6    ', vault: 658, vtrack:'Hero 4/6' },
-        '10': { loot: 655, crest: 'Gilded (16)', dtrack:'Hero 1/6    ', vault: 662, vtrack:'Myth 1/6' },
+        '8':  { loot: 652, crest: 'Gilded (12)', dtrack:'Hero 2/6    ', vault: 658, vtrack:'Hero 4/6' },
+        '9':  { loot: 652, crest: 'Gilded (14)', dtrack:'Hero 2/6    ', vault: 658, vtrack:'Hero 4/6' },
+        '10': { loot: 655, crest: 'Gilded (16)', dtrack:'Hero 3/6    ', vault: 662, vtrack:'Myth 1/6' },
     };
     const MAX_KEY_LEVEL = '10';
     const MAX_KEYS_NEEDED = 8;
